b40584a5c9ceeb3cf14b3fab1f30d3edebd217b4
technowanddev <developer02@technowand.com.au>
2021-06-11 17:47:48 +0530

    Set snackbar on ios

--- src/components/Toast/component/toast.component.tsx
+++ src/components/Toast/component/toast.component.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from '../style/toast.style';
-import {Snackbar} from 'react-native-paper';
+import { Snackbar } from 'react-native-paper';
 import { COLORS } from 'theme';
 
 type ToastProps = {
@@ -9,10 +9,10 @@ type ToastProps = {
   message: string;
 };
 
-export const Toast = ({visible, onDismiss, message}: ToastProps) => {
+export const Toast = ({ visible, onDismiss, message }: ToastProps) => {
   return (
     <>
-      <Snackbar visible={visible} onDismiss={onDismiss} style={styles.mainContainer}>
+      <Snackbar visible={visible} onDismiss={onDismiss} style={styles.mainContainer} theme={{ colors: { surface: COLORS.white } }}>
         {message}
       </Snackbar>
     </>
--- src/components/Toast/style/toast.style.ts
+++ src/components/Toast/style/toast.style.ts
@@ -1,8 +1,8 @@
-import {StyleSheet} from 'react-native';
-import {COLORS, FONTS} from 'theme';
+import { StyleSheet } from 'react-native';
+import { COLORS, FONTS } from 'theme';
 
 const styles = StyleSheet.create({
-  mainContainer: {backgroundColor:COLORS.base},
+  mainContainer: { backgroundColor: COLORS.base },
 });
 
 export default styles;
